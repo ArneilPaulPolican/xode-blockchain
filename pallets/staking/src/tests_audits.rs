817fbec8e4f6c5c32adb99006df9f3c0e5dcab82
hgminerva <hgminerva@gmail.com>
2025-11-02 08:49:43 +0800

    Test assignment operator bug https://github.com/Xode-DAO/xode-blockchain/issues/109

--- pallets/staking/src/tests_audits.rs
+++ pallets/staking/src/tests_audits.rs
@@ -1,6 +1,7 @@
 use crate::{
 	mock::*, 
 	ProposedCandidates,
+	Status,
 };
 use frame_support::{
 	assert_ok,
@@ -10,9 +11,11 @@ use frame_support::traits::{
 	Imbalance,
 	Hooks,
 };
+use pallet_session::SessionManager;
 
 // https://github.com/Xode-DAO/xode-blockchain/issues/111
 // https://github.com/Xode-DAO/xode-blockchain/issues/110
+// Test the bond_candidate
 #[test]
 fn test_pallet_xode_staking_last_updated_and_incorrect_balance_works() {
 	test1_ext().execute_with(|| {
@@ -77,11 +80,159 @@ fn test_pallet_xode_staking_last_updated_and_incorrect_balance_works() {
 }
 
 // https://github.com/Xode-DAO/xode-blockchain/issues/109
+// Test the queue_authors - a helper function that is called at the end of every session
 #[test]
 fn test_pallet_xode_staking_assignment_operator_works() {
 	test1_ext().execute_with(|| {
+		System::set_block_number(0);
+		XodeStaking::on_initialize(System::block_number());
+
         let candidate = 1;
 		assert_ok!(XodeStaking::register_candidate(RuntimeOrigin::signed(candidate)));
+
+		let imbalance = Balances::deposit_creating(&candidate, 100_000_000_000_000_000);
+		assert!(imbalance.peek() > 0, "Expected a positive imbalance for deposit creation");
+
+		assert_ok!(XodeStaking::bond_candidate(RuntimeOrigin::signed(candidate), 11_000_000_000_000_000));	
+		assert_eq!(89_000_000_000_000_000, Balances::free_balance(&candidate), "Must match");
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+
+		// The initial status must be online.  This status is set when you register a candidate
+		assert_eq!(proposed_candidates[0].status, Status::Online, "Must match");
+
+		System::set_block_number((1 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(1);
+		Session::on_initialize(System::block_number()); 
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+
+		// Bonded online candidate will be automatically wait listed
+		assert_eq!(proposed_candidates[0].status, Status::Waiting, "Must match");
+
+		System::set_block_number((2 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(2);
+		Session::on_initialize(System::block_number()); 
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+
+		// Wait listed candidate at the beginning of the session will be automatically listed as authoring
+		assert_eq!(proposed_candidates[0].status, Status::Authoring, "Must match");		
+
+		// Offline the candidate
+		let _ = XodeStaking::offline_candidate(RuntimeOrigin::signed(candidate));
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+		assert_eq!(proposed_candidates[0].offline, true, "Must match");			
+
+		System::set_block_number((3 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(3);
+		Session::on_initialize(System::block_number()); 
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+
+		// It takes two session before the author is transferred to queuing
+		assert_eq!(proposed_candidates[0].status, Status::Authoring, "Must match");			
+
+		System::set_block_number((4 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(4);
+		Session::on_initialize(System::block_number()); 
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+
+		// After two sessions the author is now transferred to queuing
+		assert_eq!(proposed_candidates[0].status, Status::Queuing, "Must match");				
+
+		// Online the candidate
+		let _ = XodeStaking::online_candidate(RuntimeOrigin::signed(candidate));
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 1, "The number of proposed candidates should be 1");		
+		assert_eq!(proposed_candidates[0].offline, false, "Must match");		
+
+		// Additional candidates, we need to maximized the proposed candidates so that the remaining
+		// waiting candidates that exceeds the maximum allowed candidates will be queued.  We only put
+		// 99 candidates to accommodate the existing candidate.
+		for i in 2..101 {
+			let _ = Balances::deposit_creating(&i, 100_000_000_000_000_000);
+			assert_ok!(XodeStaking::register_candidate(RuntimeOrigin::signed(i)));
+			assert_ok!(XodeStaking::bond_candidate(RuntimeOrigin::signed(i), 11_000_000_000_000_000));	
+		}		
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 100, "The number of proposed candidates should be 100");	
+
+		for i in 1..100 {
+			if i == 1 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Queuing, "Must match");
+			} else {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			}
+		}		
+
+		System::set_block_number((5 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(5);
+		Session::on_initialize(System::block_number()); 		
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 100, "The number of proposed candidates should be 100");	
+
+		// Three candidate remains online because of the three Xaver nodes inserted.
+		for i in 1..100 {
+			if i == 1 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Queuing, "Must match");
+			} else if i == 98 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else if i == 99 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else if i == 100 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else {
+				assert_eq!(proposed_candidates[i-1].status, Status::Waiting, "Must match");
+			}
+		}			
+
+		System::set_block_number((6 * MINUTES).into());
+		XodeStaking::on_initialize(System::block_number());
+
+		XodeStaking::new_session(6);
+		Session::on_initialize(System::block_number()); 		
+
+		let proposed_candidates = ProposedCandidates::<Test>::get();
+		assert_eq!(proposed_candidates.len(), 100, "The number of proposed candidates should be 100");	
+
+		// The queued author will immediately author because it is still listed in the waiting list.
+		// Converting queued author to waiting status at the same time set to author will happen
+		// simultaneously because the author is already in the waiting list.
+		for i in 1..100 {
+			if i == 1 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Authoring, "Must match");
+			} else if i == 98 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else if i == 99 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else if i == 100 {
+				assert_eq!(proposed_candidates[i-1].status, Status::Online, "Must match");
+			} else {
+				assert_eq!(proposed_candidates[i-1].status, Status::Authoring, "Must match");
+			}
+		}					
 	});
 }
 
